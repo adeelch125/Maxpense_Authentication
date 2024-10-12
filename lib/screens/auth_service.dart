af46a561dc066c181308fb6299f8f08006c88920
Adeel-ur-Rehman <adeelurrehman059@gmail.com>
2024-10-12 18:17:41 +0500

    auth changes

--- lib/screens/auth_service.dart
+++ lib/screens/auth_service.dart
@@ -1,34 +1,39 @@
-
-import 'dart:math';
-
+import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthService{
+class AuthService {
   final _auth = FirebaseAuth.instance;
-  Future<User?>createUserWithEmailAndPassword(String email ,String name ,String password) async{
-    try{
+
+  Future<User?> createUserWithEmailAndPassword(String email, String name, String password) async {
+    try {
       final cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       return cred.user;
-
-    }catch(e){
-      print('Something went wrong');
+    } catch (e) {
+      log('Error during registration: $e'); // Log the error message
     }
     return null;
   }
 
-  Future<User?>loginUserWithEmailAndPassword(String email , String password) async{
-    try{
+  Future<User?> loginUserWithEmailAndPassword(String email, String password) async {
+    try {
       final cred = await _auth.signInWithEmailAndPassword(email: email, password: password);
       return cred.user;
-
-    }catch(e){
+    } catch (e) {
+      log('Error during login: $e'); // Log the error message
 
     }
     return null;
   }
- Future<void>signOut()async{
-    try{
+
+  Future<void> signOut() async {
+    try {
       await _auth.signOut();
-    }catch(e){}
- }
+    } catch (e) {
+      log('Error during sign-out: $e'); // Log the error message
+    }
+  }
+
+  signInWithGoogle() {
+    // Add Google sign-in logic
+  }
 }
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:max_pense/screens/auth_service.dart';
-
 import '../assets/assets.dart';
 import '../colors/app_colors.dart';
 import 'login_screen.dart';
@@ -16,15 +16,17 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   final _auth = AuthService();
-  final _email = TextEditingController();
-  final _name = TextEditingController();
-  final _password = TextEditingController();
+  final _emailController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
 
   @override
   void dispose() {
-    _email.dispose();
-    _name.dispose();
-    _password.dispose();
+    _emailController.dispose();
+    _nameController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
@@ -35,171 +37,147 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: Padding(
         padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
         child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Image(image: AssetImage(Assets.backButton)),
-              const SizedBox(height: 3),
-              const Text(
-                'Register',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.blue),
-              ),
-              const SizedBox(height: 7),
-              const Text(
-                'Create an account to access all the features of Maxpense!',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 15),
-              const Text('Email', style: TextStyle(fontSize: 16)),
-              const SizedBox(height: 5),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                controller: _email,
-                decoration: InputDecoration(
-                  hintText: 'Ex: abc@example.com',
-                  hintStyle: const TextStyle(color: AppColors.hintColor),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset(
-                      Assets.iconEmail,
-                      width: 24,
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 1.5,
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 1.5,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text('Your Name', style: TextStyle(fontSize: 16)),
-              const SizedBox(height: 5),
-              TextFormField(
-                controller: _name,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  hintText: 'Ex. Saul Ramirez',
-                  hintStyle: const TextStyle(color: AppColors.hintColor),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset(
-                      Assets.imgName,
-                      width: 24,
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 1.5,
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 1.5,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text('Your Password', style: TextStyle(fontSize: 16)),
-              const SizedBox(height: 5),
-              TextFormField(
-                controller: _password,
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter your password',
-                  hintStyle: const TextStyle(color: AppColors.hintColor),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset(
-                      Assets.imgPassword,
-                      width: 24,
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 1.5,
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 1.5,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: TextButton(
-                  onPressed: () => _signUp(context),
-                  child: const Text(
-                    'Register',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 21),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+          child: SingleChildScrollView(  // Wrap in SingleChildScrollView
+            child: Form(
+              key: _formKey, // Add Form widget for validation
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text('Already have an account?', style: TextStyle(fontSize: 16)),
-                  const SizedBox(width: 5),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+                  const Image(image: AssetImage(Assets.backButton)),
+                  const SizedBox(height: 3),
+                  const Text(
+                    'Register',
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue),
+                  ),
+                  const SizedBox(height: 7),
+                  const Text(
+                    'Create an account to access all the features of Maxpense!',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 15),
+                  const Text('Email', style: TextStyle(fontSize: 16)),
+                  const SizedBox(height: 5),
+                  TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an email';
+                      } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                        return 'Enter a valid email';
+                      }
+                      return null;
                     },
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),
+                    decoration: InputDecoration(
+                      hintText: 'Ex: abc@example.com',
+                      hintStyle: const TextStyle(color: AppColors.hintColor),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          Assets.iconEmail,
+                          width: 24,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: const BorderSide(color: Colors.blue, width: 1.5),
+                      ),
                     ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text('Your Name', style: TextStyle(fontSize: 16)),
+                  const SizedBox(height: 5),
+                  TextFormField(
+                    controller: _nameController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Ex: Saul Ramirez',
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(Assets.imgName, width: 24),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: const BorderSide(color: Colors.blue, width: 1.5),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text('Password', style: TextStyle(fontSize: 16)),
+                  const SizedBox(height: 5),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      } else if (value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Enter your password',
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(Assets.imgPassword, width: 24),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: const BorderSide(color: Colors.blue, width: 1.5),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: TextButton(
+                      onPressed: _isLoading ? null : () => _signUp(context),
+                      child: _isLoading
+                          ? const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      )
+                          : const Text(
+                        'Register',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 21),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Already have an account?', style: TextStyle(fontSize: 16)),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context, MaterialPageRoute(builder: (context) => LoginScreen()));
+                        },
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
@@ -207,10 +185,33 @@ class _RegisterScreenState extends State<RegisterScreen> {
   }
 
   void _signUp(BuildContext context) async {
-    final user = await _auth.createUserWithEmailAndPassword(_email.text, _name.text, _password.text);
-    if (user != null) {
-      log("User created successfully");
-      goToHome(context);
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      try {
+        final user = await _auth.createUserWithEmailAndPassword(
+            _emailController.text.trim(),
+            _passwordController.text.trim(),
+            _nameController.text.trim()
+        );
+        if (user != null) {
+          Fluttertoast.showToast(msg: 'Success',toastLength:Toast.LENGTH_SHORT);
+          goToHome(context);
+        }
+        else{
+          Fluttertoast.showToast(msg: "Error",toastLength: Toast.LENGTH_SHORT);
+        }
+      } catch (e) {
+
+        log("Error: $e");
+        // You can show a message using Fluttertoast or any other package
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
     }
   }
 
